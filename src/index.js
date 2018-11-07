cc10c0ff03b982a34bd14fee34cbba332ed3440d
Stefan B <codingsteff@users.noreply.github.com>
2018-11-07 11:32:39 +0100

    show more

--- src/index.js
+++ src/index.js
@@ -7,3 +7,10 @@ require("file-loader?name=web.config!./web.config");
 
 document.getElementById('profile').src = require('./img/profile.png');
 document.getElementById("year").innerHTML = (new Date()).getFullYear();
+
+function showMoreWhoAmi() {
+    document.getElementById('whoami-more-button').style.visibility = 'hidden';
+    document.getElementById('whoami-more').style.visibility = 'visible';
+}
+
+document.getElementById('whoami-more-button').addEventListener('click', showMoreWhoAmi);
