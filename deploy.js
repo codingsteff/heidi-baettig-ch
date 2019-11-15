9550234a7df85affa33c26401e3ea76dcc820733
Stefan B <codingsteff@users.noreply.github.com>
2019-11-15 16:04:12 +0100

    ftp-deploy script

--- deploy.js
+++ deploy.js
@@ -1,1 +1,19 @@
-console.log('secret user', process.env.FTP_USER);
+var FtpDeploy = require('ftp-deploy');
+var ftpDeploy = new FtpDeploy();
+
+var config = {
+    user: process.env.FTP_USER,
+    password: process.env.FTP_PW,
+    host: process.env.FTP_HOST,
+    port: 21,
+    localRoot: __dirname + '/dist',
+    remoteRoot: '/httpdocs/',
+    include: ['*', '**/*'], // this would upload everything except dot files
+    deleteRemote: true,
+    forcePasv: true // Passive mode is forced (EPSV command is not sent)
+};
+
+// use with promises
+ftpDeploy.deploy(config)
+    .then(res => console.log('finished:', res))
+    .catch(err => console.log(err))
