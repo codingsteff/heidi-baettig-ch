4bf2546e5505181e05a79d2f6f06ff328cecd76c
Stefan B <codingsteff@users.noreply.github.com>
2019-11-15 16:16:20 +0100

    formatted

--- webpack.config.js
+++ webpack.config.js
@@ -3,48 +3,48 @@ const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    module: {
-        rules: [{
-                test: /\.scss$/,
-                use: [{
-                    loader: "style-loader" // creates style nodes from JS strings
-                }, {
-                    loader: "css-loader" // translates CSS into CommonJS
-                }, {
-                    loader: "sass-loader" // compiles Sass to CSS
-                }]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            }
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlwebpackPlugin({
-            template: 'src/index.html',
-            favicon: 'src/favicon.ico',
-            inlineSource: '.(js|css)$', // embed all javascript and css inline needs inline-source-plugin
-            minify: {
-                collapseWhitespace: true
-            }
-        }),
-        new HtmlWebpackInlineSourcePlugin()
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
     ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlwebpackPlugin({
+      template: 'src/index.html',
+      favicon: 'src/favicon.ico',
+      inlineSource: '.(js|css)$', // embed all javascript and css inline needs inline-source-plugin
+      minify: {
+        collapseWhitespace: true
+      }
+    }),
+    new HtmlWebpackInlineSourcePlugin()
+  ]
 };
