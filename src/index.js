82bf4ad72bdffa7f57da47203b93faa036514aea
Stefan B <codingsteff@users.noreply.github.com>
2019-12-05 14:36:47 +0100

    profile foto added

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import './index.scss';
 // Kopien für /dist
 require("file-loader?name=web.config!./web.config");
 
-document.getElementById('profile').src = require('./img/profile.png');
+import imgProfle from './img/profile.png';
+document.getElementById('profile').src = imgProfle;
 document.getElementById("year").innerHTML = (new Date()).getFullYear();
 
 function showMoreWhoAmi() {
     document.getElementById('whoami-more-button').style.display = 'none';
-    document.getElementById('whoami-more').style.visibility = 'visible';
+    document.getElementById('whoami-more').style.display = 'block';
+    document.getElementById('profile').style.marginBottom = 0;
 }
 
 document.getElementById('whoami-more-button').addEventListener('click', showMoreWhoAmi);
